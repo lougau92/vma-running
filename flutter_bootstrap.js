51df653fadbe1220148b07886dc0f43c30215038
lougau92 <lougau92@users.noreply.github.com>
2025-12-14 13:42:21 +0000

    deploy: 4c0e38c546e7a3d0feba9240b3a541e3df0658fb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4046453661"
+    serviceWorkerVersion: "4218551088"
   }
 });
